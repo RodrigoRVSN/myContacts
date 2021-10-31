d162b26a162cca2842c8c0767d3fd9f710974b2c
RodrigoRVSN <rodrigovictor81@gmail.com>
2021-10-31 15:06:43 -0300

    🔨 treatment in error by contentType

--- frontend/src/services/utils/HttpClient.js
+++ frontend/src/services/utils/HttpClient.js
@@ -6,14 +6,23 @@ class HttpClient {
   }
 
   async get(path) {
-    const response = await fetch(`${this.baseURL}${path}`);
-
     await delay(500);
-    if (response.ok) {
-      return response.json();
+
+    const response = await fetch(`${this.baseURL}${path}`);
+    const contentType = response.headers.get('Content-Type');
+
+    let body = null;
+    if (contentType.includes('application/json')) {
+      body = await response.json();
     }
 
-    throw new Error(`${response.status} - ${response.statusText}`);
+    if (response.ok) {
+      return body;
+    }
+
+    throw new Error(
+      body?.error || `${response.status} - ${response.statusText}`,
+    );
   }
 }
 export default HttpClient;
