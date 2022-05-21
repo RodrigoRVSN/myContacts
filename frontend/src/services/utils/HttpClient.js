591dd6fcf7bc74fc1357e9de85057699065e83bd
RodrigoRVSN <rodrigovictor81@gmail.com>
2022-05-20 22:13:55 -0300

    refactor: http client method to make request

--- frontend/src/services/utils/HttpClient.js
+++ frontend/src/services/utils/HttpClient.js
@@ -6,34 +6,41 @@ class HttpClient {
     this.baseURL = baseURL;
   }
 
-  async get(path) {
-    await delay(500);
-
-    const response = await fetch(`${this.baseURL}${path}`);
-    const contentType = response.headers.get('Content-Type');
-
-    let body = null;
-    if (contentType.includes('application/json')) {
-      body = await response.json();
-    }
-
-    if (response.ok) {
-      return body;
-    }
-
-    throw new APIError(response, body);
+  async get(path, options) {
+    return this.makeRequest(path, {
+      method: 'GET',
+      headers: options?.headers,
+    });
   }
 
-  async post(path, body) {
+  async post(path, options) {
+    return this.makeRequest(path, {
+      method: 'POST',
+      body: options?.body,
+      headers: options?.headers,
+    });
+  }
+
+  async makeRequest(path, options) {
     await delay(500);
 
-    const headers = new Headers({
-      'Content-Type': 'application/json',
-    });
+    const headers = new Headers();
+
+    if (options.body) {
+      headers.append(
+        'Content-Type', 'application/json',
+      );
+    }
+
+    if (options.headers) {
+      Object.entries(options.headers).forEach(([name, value]) => {
+        headers.append(name, value);
+      });
+    }
 
     const response = await fetch(`${this.baseURL}${path}`, {
-      method: 'POST',
-      body: JSON.stringify(body),
+      method: options.method,
+      body: JSON.stringify(options.body),
       headers,
     });
 
@@ -50,4 +57,5 @@ class HttpClient {
     throw new APIError(response, responseBody);
   }
 }
+
 export default HttpClient;
