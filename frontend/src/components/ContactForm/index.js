e5d35d0a9a635c854d72a0757a4929e8e05db621
RodrigoRVSN <rodrigovictor81@gmail.com>
2022-07-23 19:43:38 -0300

    feat: custom hook to handle state in unmount to kill memory leak

--- frontend/src/components/ContactForm/index.js
+++ frontend/src/components/ContactForm/index.js
@@ -12,14 +12,15 @@ import isEmailValid from '../../utils/isEmailValid';
 import useErrors from '../../hooks/useErrors';
 import formatPhone from '../../utils/formatPhone';
 import CategoriesService from '../../services/CategoriesService';
+import { useSafeAsyncState } from '../../hooks/useSafeAsyncState';
 
 export const ContactForm = forwardRef(({ buttonLabel = '', onSubmit }, ref) => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [phone, setPhone] = useState('');
   const [categoryId, setCategoryId] = useState('');
-  const [categories, setCategories] = useState([]);
-  const [isLoadingCategories, setIsLoadingCategories] = useState(true);
+  const [categories, setCategories] = useSafeAsyncState([]);
+  const [isLoadingCategories, setIsLoadingCategories] = useSafeAsyncState(true);
   const [isSubmitting, setIsSubmitting] = useState(false);
 
   const {
